b108a088d1611916f0322c37f5bd6a28f71405fd
Franck Albinet <franckalbinet@gmail.com>
2016-09-28 11:45:22 +0200

    updated build file

--- gis/animal-health/ghana-2016/build.sh
+++ gis/animal-health/ghana-2016/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# to convert .md files to pdf
+# pandoc -f markdown_github -t beamer agenda.md -o agenda.pdf
+
 # copy files from master course
 cp ../master-course/src/resources.md ./src/
 cp ../master-course/src/qgis-setup.md ./src/
